1bb4acc06b9ca8d0508ba8b0c30b09e53fdc791f
clark rivas <clarkrivas108@gmail.com>
2025-02-20 03:13:57 +0800

    font setup and icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,33 @@
-import { Stack } from "expo-router";
-import "./global.css";
-
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import "./global.css"
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+    "SpaceMono-Regular": require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  // ✅ Prevent splash screen from hiding automatically
+  useEffect(() => {
+    SplashScreen.preventAutoHideAsync();
+  }, []);
+
+  // ✅ Hide splash screen when fonts are loaded
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  // ✅ Show nothing until fonts are loaded
+  if (!fontsLoaded) return null;
+
+  return <Stack screenOptions={{headerShown: false}}/>;
 }
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const SigIn = () => {
   return (
-    <View>
-      <Text>SigIn</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text className='Text-base text-center uppercase font-rubik text-black-200'>Try</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
